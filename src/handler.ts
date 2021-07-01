c398245398473f863001fcb57d7c06c8280e4015
Peh Zi Heng <pehziheng@gmail.com>
2021-07-01 19:45:06 +0800

    Fix function

--- src/handler.ts
+++ src/handler.ts
@@ -1,19 +1,6 @@
-import User from "./models/User";
+import {User} from "./models/User";
 import {AGConnectCloudDB, CloudDBZoneConfig} from "@agconnect/database-server";
 import {AGCClient, CredentialParser} from "@agconnect/common-server";
-
-// Init AGCClient
-const credentialPath = "/dcache/layer/func/resource/agc-apiclient-660110761581888128-6979164466034395931.json";
-AGCClient.initialize(CredentialParser.toCredential(credentialPath));
-
-// Init CloudDB
-const agcClient = AGCClient.getInstance();
-AGConnectCloudDB.initialize(agcClient);
-
-// Define and create zone
-const zoneName = 'Foundvio';
-const cloudDBZoneConfig = new CloudDBZoneConfig(zoneName);
-const mCloudDBZone = AGConnectCloudDB.getInstance().openCloudDBZone(cloudDBZoneConfig);
 
 let addUser = async function (event, context, callback, logger) {
 
@@ -39,21 +26,21 @@ let addUser = async function (event, context, callback, logger) {
         user.setIsTracker(_body.isTracker)
 
         // Log results
-        logger.info("PhoneId: ", user.getPhoneId())
-        logger.info("Given Name: ", user.getGivenName())
-        logger.info("Family Name: ", user.getFamilyName())
-        logger.info("isTracker: ", user.getIsTracker())
+        logger.info("PhoneId: " + user.getPhoneId())
+        logger.info("Given Name: " + user.getGivenName())
+        logger.info("Family Name: " + user.getFamilyName())
+        logger.info("isTracker: " + user.getIsTracker())
 
         // Perform Upsert
         try {
 
             let response = await upsertUser(user)
-            logger.info("Upsert Successful => ", response)
+            logger.info("Upsert Successful => " + response)
             res.body = "Success"
 
         } catch (error) {
 
-            logger.error("Upsert Failed => ", error)
+            logger.error("Upsert Failed => " + error)
             res.body = "Error"
         }
 
@@ -65,17 +52,27 @@ let addUser = async function (event, context, callback, logger) {
     }
 
     // Callback
-    context.callback(res)
+    context.callback(res);
 
     // Upsert Function
     async function upsertUser(user) {
+        // Init AGCClient
+        const credentialPath = "/dcache/layer/func/resource/agc-apiclient-660110761581888128-6979164466034395931.json";
+        AGCClient.initialize(CredentialParser.toCredential(credentialPath));
+
+        // Init CloudDB
+        const agcClient = AGCClient.getInstance();
+        AGConnectCloudDB.initialize(agcClient);
+
+        // Define and create zone
+        const zoneName = 'Foundvio';
+        const cloudDBZoneConfig = new CloudDBZoneConfig(zoneName);
+        const mCloudDBZone = AGConnectCloudDB.getInstance().openCloudDBZone(cloudDBZoneConfig);
         if (!mCloudDBZone) {
             console.log("CloudDBClient is null, try re-initialize it");
             return;
         }
-
-        const resp = await mCloudDBZone.executeUpsert(user);
-        return resp;
+        return await mCloudDBZone.executeUpsert(user);
     }
 
 }
